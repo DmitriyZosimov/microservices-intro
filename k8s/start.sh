8ee6d6cf59a89a73950a256e76e5932ef99ade9b
DmitriyZosimov <dmitriy_zosimov@epam.com>
2025-10-02 18:33:03 +0300

    Sub-task 2: Deploy containers in k8s and Sub-task 4: Stateful Sets

---
+++ k8s/start.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#echo "Start building the application..."
+#cd ..
+#./gradlew -q clean build -x test
+#echo "The application build is completed"
+#
+#echo "Build docker images..."
+#
+#cd resources-service
+#docker build -t resource-image -f ./Dockerfile .
+#
+#cd ../songs-service
+#docker build -t song-image -f ./Dockerfile .
+#
+#echo "Loading images..."
+#minikube image load resource-image:latest
+#minikube image load song-image:latest
+#
+#echo "Starting k8s"
+#cd ../k8s
+kubectl apply -f namespace.yaml
+if kubectl get configmap db-config -n k8s-program; then
+  echo "ConfigMap db-config already exists. Updating..."
+  kubectl delete configmap db-config -n k8s-program
+fi
+kubectl create configmap db-config --from-env-file=../.env -n k8s-program
+
+echo "Deploying resources..."
+kubectl apply -f resources-service/ -f songs-service/ || { echo "Failed to deploy services"; exit 1; }
+echo "Services have started!"
+
