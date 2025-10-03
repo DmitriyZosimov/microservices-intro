e7afd5f2ed4fa836baecb33c595a839063524a26
Dmitriy <87096865+DmitriyZosimov@users.noreply.github.com>
2025-10-03 17:25:42 +0300

    Update start.sh

--- k8s/start.sh
+++ k8s/start.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-#echo "Start building the application..."
-#cd ..
-#./gradlew -q clean build -x test
-#echo "The application build is completed"
-#
-#echo "Build docker images..."
-#
-#cd resources-service
-#docker build -t resource-image -f ./Dockerfile .
-#
-#cd ../songs-service
-#docker build -t song-image -f ./Dockerfile .
-#
-#echo "Loading images..."
-#minikube image load resource-image:latest
-#minikube image load song-image:latest
-#
-#echo "Starting k8s"
-#cd ../k8s
+echo "Start building the application..."
+cd ..
+./gradlew -q clean build -x test
+echo "The application build is completed"
+
+echo "Build docker images..."
+
+cd resources-service
+docker build -t resource-image -f ./Dockerfile .
+
+cd ../songs-service
+docker build -t song-image -f ./Dockerfile .
+
+echo "Loading images..."
+minikube image load resource-image:latest
+minikube image load song-image:latest
+
+echo "Starting k8s"
+cd ../k8s
 kubectl apply -f namespace.yaml
 if kubectl get configmap db-config -n k8s-program; then
   echo "ConfigMap db-config already exists. Updating..."
