b74825e4d5b4a428b7f1210601493c9b854f73a8
DmitriyZosimov <dmitriy_zosimov@epam.com>
2025-10-08 16:56:09 +0300

    Move songs-service to another namespace

--- k8s/start.sh
+++ k8s/start.sh
@@ -17,16 +17,27 @@ echo "Loading images..."
 minikube image load resource-image:latest
 minikube image load song-image:latest
 
-echo "Starting k8s"
-cd ../k8s
-kubectl apply -f namespace.yaml
+echo "Starting Songs Service..."
+cd ../k8s/songs-service
+kubectl apply -f songs-service-namespace.yaml
+
+if kubectl get configmap db-config -n songs-service; then
+  echo "ConfigMap db-config already exists in songs-service. Updating..."
+  kubectl delete configmap db-config -n songs-service
+fi
+kubectl create configmap db-config --from-env-file=../../.env -n songs-service
+kubectl apply -f ./ || { echo "Failed to deploy Songs services"; exit 1; }
+
+echo "Starting Resource Service..."
+cd ../resources-service
+kubectl apply -f resource-services-namespace.yaml
 if kubectl get configmap db-config -n k8s-program; then
-  echo "ConfigMap db-config already exists. Updating..."
+  echo "ConfigMap db-config already exists in k8s-program. Updating..."
   kubectl delete configmap db-config -n k8s-program
 fi
-kubectl create configmap db-config --from-env-file=../.env -n k8s-program
 
-echo "Deploying resources..."
-kubectl apply -f resources-service/ -f songs-service/ || { echo "Failed to deploy services"; exit 1; }
+kubectl create configmap db-config --from-env-file=../../.env -n k8s-program
+
+kubectl apply -f ./ || { echo "Failed to deploy Resource services"; exit 1; }
 echo "Services have started!"
 
